92685c40c5dbf9504036801cf50c399f01f5c83f
raindou <wwweijiao@gmail.com>
2023-05-31 17:53:32 +0800

    init hello world

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,16 @@
+/**
+ * Hello world
+ *
+ */
+
+public class HelloWorld {
+
+  public static void main(String[] args) {
+    System.out.println("Hello world raindou!");
+  }
+
+  public static void greet(String name) {
+      System.out.println(String.format("Hello %s!", name));
+  }
+
+}
