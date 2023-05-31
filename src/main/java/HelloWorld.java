40ba23f5c617dc28ded3895874b32459da860186
Raindou <wwweijiao@gmail.com>
2023-05-31 17:56:46 +0800

    Update HelloWorld.java

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -6,7 +6,7 @@
 public class HelloWorld {
 
   public static void main(String[] args) {
-    System.out.println("Hello world raindou!");
+    System.out.println("Hello world from the outside!");
   }
 
   public static void greet(String name) {
